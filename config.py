018dcd890faabf8f34dff7718a1c1e55d2576302
AliQambari <43219682+AliQambari@users.noreply.github.com>
2022-10-25 12:38:35 +0330

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv(find_dotenv())
 
 
 def get_dropbox_access_token():
-    return os.environ.get("sl.BRzn7if2jRs-o9Okm1qWAa6nJ17Ux88cbVb8aO6feEzfJuGhfpA42NzHM-_avWMgHfe2_1YcHQMAcsFjHyHRE5GhuDTAtg7QUOzu7fvGQT6stq3E_G7aBF6ycIoYUlYZASIQBeAxZeE", "h9r209copjs6qtt")
+    return os.environ.get("sl.BRzn7if2jRs-o9Okm1qWAa6nJ17Ux88cbVb8aO6feEzfJuGhfpA42NzHM-_avWMgHfe2_1YcHQMAcsFjHyHRE5GhuDTAtg7QUOzu7fvGQT6stq3E_G7aBF6ycIoYUlYZASIQBeAxZeE", "")
