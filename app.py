a6fca39e405684eed0e666be6e5a3654ce3c093e
AliQambari <43219682+AliQambari@users.noreply.github.com>
2022-10-25 12:05:18 +0330

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ import uuid
 
 app = Flask(__name__)
 app.secret_key = "@aqa96df#AQAQ"
-dbx = dropbox.Dropbox("sl.BRzn7if2jRs-o9Okm1qWAa6nJ17Ux88cbVb8aO6feEzfJuGhfpA42NzHM-_avWMgHfe2_1YcHQMAcsFjHyHRE5GhuDTAtg7QUOzu7fvGQT6stq3E_G7aBF6ycIoYUlYZASIQBeAxZeE")
-
+dbx = dropbox.Dropbox(get_dropbox_access_token())
 
 @app.route("/")
 def welcome():
