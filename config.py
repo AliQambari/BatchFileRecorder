7376d26b7b1f430862fd5e2a51a781bc477d4a7f
AliQambari <43219682+AliQambari@users.noreply.github.com>
2022-10-25 21:28:19 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,22 @@
 import os
-
+import requests
+from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 
+
+app_key = "avs1fp3wryz7rxu"
+app_secret = "lywdvlrgk4vho1i"
+refresh_token = "0S_uYLg3zk4AAAAAAAAAAfILUG-CAZRAV5fWg1FElyBZz1fkeiLLFfaygESu240I"
+
+
+data = {'refresh_token': refresh_token, 'grant_type': 'refresh_token'}
+
+r = requests.post('https://api.dropbox.com/oauth2/token', data=data,
+auth = HTTPBasicAuth(app_key, app_secret))
+result = r.json()
+result = result["access_token"]
 def get_dropbox_access_token():
-    return os.environ.get("sl.BRzn7if2jRs-o9Okm1qWAa6nJ17Ux88cbVb8aO6feEzfJuGhfpA42NzHM-_avWMgHfe2_1YcHQMAcsFjHyHRE5GhuDTAtg7QUOzu7fvGQT6stq3E_G7aBF6ycIoYUlYZASIQBeAxZeE", "")
+    return result
